6f70626c99224a53c8426f36f2c2ad56d2986133
John Wregglesworth <wregglej@cyverse.org>
2022-04-08 14:13:19 -0700

    Fix a broken down migration

--- migrations/000020_permanent_id_request_types.down.sql
+++ migrations/000020_permanent_id_request_types.down.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 SET search_path = public, pg_catalog;
 
-DROP TABLE IF EXISTS permanent_id_request_statuses;
+DROP INDEX IF EXISTS permanent_id_request_types_unique;
+DROP TABLE IF EXISTS permanent_id_request_types;
 
 COMMIT;
